b99a588638552ce10181651db6f6522af10d2319
Thegegors <gweiss0@chicagobooth.edu>
2023-03-29 04:34:36 +0000

    First Development Basic page /navbar functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
-
-
+  get "about", to: "about#index"
+  get "team", to: "team#index"
+  get "opportunities", to: "opportunities#index"
+  get "contact", to: "contact#index"
+  root to: "main#index"
 end
